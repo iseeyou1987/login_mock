cf43a9d37db72a5ea04fbd92b7c682700f87bfbd
zhangdapeng89 <zhangdapeng89@126.com>
2015-06-05 16:13:36 +0800

    添加测试模块

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 
 var koa = require('koa');
 var router = require('./router');
+var logger = require('koa-logger')
 
 var app = koa();
 
+app.use(logger());
 app.use(router.routes());
 
 app.listen(3050);
