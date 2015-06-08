f30b83780384fdc045d53cb755352066f397dbcf
durban <zhangdapeng89@126.com>
2015-06-08 20:21:33 +0800

    添加配置文件

---
+++ config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+  mysql : {
+    hostname: "127.0.0.1",
+    username: "root",
+    password: "123456",
+    database: "qeeniao",
+    port : 3306,
+    poolSize : 10
+  }
+};
