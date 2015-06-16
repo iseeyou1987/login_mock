23257a9b7afe912860a42376894534d83108e532
durban <zhangdapeng89@126.com>
2015-06-16 19:45:19 +0800

    添加139邮箱fetcher测试

---
+++ test/test_139_fetcher.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var Fetcher = require('../lib/mail_fetcher/139').Fetcher;
+var co = require('co');
+var fs = require('fs');
+var thunkify = require('thunkify');
+var readFile = thunkify(fs.readFile);
+var eventWrap = require('co-event-wrap');
+var should = require('should');
+var net139 = require('../mockers/139');
+var debug = require('debug');
+
+
+var username = 'xxxxxxxxxxxx@139.com';
+var password = 'xxxxxxxxxxxxx';
+
+describe('139 Fetcher',function(){
+  it('test list',function *(){
+    var cookie = '';
+    try{
+      cookie = yield net139.getCookie(username,password);
+    }catch(e){
+      console.log('Get Cookie Error:',e);
+    }
+
+    var fetcher = new Fetcher({'cookie':cookie});
+    var ev = eventWrap(fetcher);
+
+    var ev = eventWrap(fetcher);
+    ev.on('message', function* (data) {
+      data.should.have.property('subject');
+      data.should.have.property('content');
+      data.should.have.property('date');
+      
+      console.log('data:',data['content']);
+    });
+
+    ev.on('error', function* (data) {
+      console.log('Error:',data);
+    });
+
+    ev.on('end', function* (data){
+      console.log('End:',data);
+    });
+
+    var res = yield fetcher.getContent();
+  });
+});
