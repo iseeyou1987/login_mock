e519044d01d703fcfd0d913a174f616428cc038d
durban <zhangdapeng89@126.com>
2015-06-16 19:47:11 +0800

    添加139邮箱账单导入测试

---
+++ test/test_import_139_email.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var co = require('co');
+var fs = require('fs');
+var eventWrap = require('co-event-wrap');
+var should = require('should');
+var ImportEmail = require('../tasks/importEmail');
+var debug = require('debug')('test_import_email');
+
+describe('Import Email Test',function(){
+  it('139 login and fetcher email',function *(){
+    try{
+      var res = yield ImportEmail(49792);
+    }catch(e){
+      debug(e.stack);
+    }
+  });
+});
