8d896eb48ac2f89531856e182a2202b58ba7b421
durban <zhangdapeng89@126.com>
2015-06-17 10:28:52 +0800

    添加新浪fetcher的测试

---
+++ test/test_sina_fetcher.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Fetcher = require('../lib/mail_fetcher/sina').Fetcher;
+var co = require('co');
+var fs = require('fs');
+var thunkify = require('thunkify');
+var readFile = thunkify(fs.readFile);
+var eventWrap = require('co-event-wrap');
+var should = require('should');
+var netsina = require('../mockers/sina');
+var moment = require('moment');
+var debug = require('debug')('sina_fetcher');
+
+var username = 'zhangdapeng89@sina.cn';
+var password = 'QWE123;;';
+
+describe('sina Fetcher',function(){
+  it('test list',function *(){
+    try{
+      var cookie = yield netsina.getCookie(username,password);
+    }catch(e){
+      console.log('message:',e.message);
+      debug(e.stack);
+      return;
+    }
+
+    var fetcher = new Fetcher({'cookie':cookie,'date':moment('2015-03-26').toDate()});
+    var ev = eventWrap(fetcher);
+
+    ev.on('message', function* (data) {
+      data.should.have.property('subject');
+      data.should.have.property('content');
+      data.should.have.property('date');
+    });
+
+    ev.on('error', function* (error) {
+      console.log('Error:',error.stack);
+    });
+
+    ev.on('end', function* (data){
+      console.log('End:',data);
+    });
+
+    var res = yield fetcher.getContent();
+  });
+});
