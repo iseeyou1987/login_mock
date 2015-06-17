f07ec1270beb64ae72b75c5be97eaab669079e2d
durban <zhangdapeng89@126.com>
2015-06-17 10:25:59 +0800

    给139模拟登录添加debug

--- test/test_139_login.js
+++ test/test_139_login.js
@@ -1,20 +1,21 @@
 'use strict';
 var co = require('co');
 var net139 = require('../mockers/139');
-var username = 'xxxxxxxxx@139.com';
-var password = 'zzzzzzzzzzzz';
 var fs = require('fs');
 var thunkify = require('thunkify');
 var writeFile = thunkify(fs.writeFile);
+var debug = require('debug')('139_login');
+
+var username = 'xxxxxxxxx@139.com';
+var password = 'zzzzzzzzzzzz';
 
 describe('139 login',function(){
   it('get cookie',function *(){
     try{
       var cookie = yield net139.getCookie(username,password);
-      yield writeFile(__dirname+'/139_cookie.txt',cookie);
       console.log(cookie);
     }catch(e){
-      console.log(e.message);
+      debug(e.stack);
     }
   });
 });
