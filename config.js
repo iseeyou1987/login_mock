97615b3996c4c2ca3645c940d34d6cca60568cc5
durban <zhangdapeng89@126.com>
2015-07-01 10:02:24 +0800

    修改config 添加log日志配置

--- config.js
+++ config.js
@@ -8,5 +8,31 @@ module.exports = {
     database: "qeeniao",
     port : 3306,
     poolSize : 10
-  }
+  },
+  logDir: __dirname + '/log/',
+  log4js: {
+    appenders: [
+      {
+        type: 'file',
+        category: 'worker',
+        filename: 'worker.log',
+        maxLogSize: 1024 * 1024 * 2,
+        backups: 3
+      },
+      {
+        type: 'file',
+        category: 'api',
+        filename: 'api.log',
+        maxLogSize: 1024 * 1024 * 2,
+        backups: 3
+      },
+      {
+        type: 'file',
+        category: 'task',
+        filename: 'task.log',
+        maxLogSize: 1024 * 1024 * 2,
+        backups: 3
+      },
+    ]
+  },
 };
